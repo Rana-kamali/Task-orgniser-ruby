51d51cbe13c20d95597e6842efeef05407a81d1a
Rana-Kamali <raana.kamalinezhad@gmail.com>
2021-03-25 18:03:14 +1100

    set up files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { Survey } from './component/Survey';
+
+
+function App() {
+  return (
+    <div className="App">
+  <Survey/>
+    </div>
+  );
+}
+
+export default App;
