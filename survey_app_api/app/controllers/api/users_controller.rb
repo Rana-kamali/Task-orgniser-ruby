f891aa54227a38d078d68ffb00f5a73eb952da82
Rana-Kamali <raana.kamalinezhad@gmail.com>
2021-03-26 11:19:12 +1100

    Update user is working

--- survey_app_api/app/controllers/api/users_controller.rb
+++ survey_app_api/app/controllers/api/users_controller.rb
@@ -1,17 +1,22 @@
 class Api::UsersController < ApplicationController
-    def index
-      render json: User.all
-    end
-def user_params
+  def index
+    render json: User.all
+  end
+
+  def user_params
     params.required(:user).permit(:name, :password)
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: user, status: 201
+    else
+      render json: { message: 'unable to create a user' }, status: 500
+    end
+  end
+def update
+    render json: User.find(params[:id]).update(user_params)
 end
 
-    def create
-        user = User.create(user_params)
-        if user.valid?
-            render json: user, status: 201
-        else
-            render json: {message: 'unable to create a user'}, status: 500
-        end
-    end
 end
