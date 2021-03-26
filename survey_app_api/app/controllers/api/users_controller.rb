92c426a4e972be7f5f3306171d7b8ff18552ff69
Rana-Kamali <raana.kamalinezhad@gmail.com>
2021-03-26 11:14:45 +1100

    Create a new user

--- survey_app_api/app/controllers/api/users_controller.rb
+++ survey_app_api/app/controllers/api/users_controller.rb
@@ -2,4 +2,16 @@ class Api::UsersController < ApplicationController
     def index
       render json: User.all
     end
+def user_params
+    params.required(:user).permit(:name, :password)
+end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user, status: 201
+        else
+            render json: {message: 'unable to create a user'}, status: 500
+        end
+    end
 end
